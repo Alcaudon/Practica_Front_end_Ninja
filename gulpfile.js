4fade650f4e9aea8b526d5d0c3548b84292772c0
Luis Barriga <barbaclu@gmail.com>
2017-08-10 19:46:10 +0200

    Mejoras en el gulpfile.js añadiendo notify y browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require ("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
+var notify = require("gulp-notify");
 
 gulp.task("default", function(){
-    console.log("Hola mundo");
+    browserSync.init({server: "src/"});
+    gulp.watch(["src/scss/*.scss","src/scss/**/*.scss"],["sass"]);
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 })
 
 gulp.task("sass", function(){
-    console.log("Compilando SASS bien");
+    gulp.src("src/scss/style.scss")
+        .pipe(sass().on("error", function(error){
+            return notify().write(error);
+        }))
+        .pipe(gulp.dest("src/css/"))
+        .pipe(browserSync.stream());
 })
