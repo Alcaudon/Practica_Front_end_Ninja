a713e6855582aeaaf06d031224b1c88d890d08a5
Luis Barriga <barbaclu@gmail.com>
2017-08-17 20:59:41 +0200

    Definimos la tarea de compilación JS.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,16 @@ var sass = require("gulp-sass");
 var browserSync = require("browser-sync").create();
 var notify = require("gulp-notify");
 var gulpImport = require("gulp-html-import");
+var tap = require("gulp-tap");
+var browserify = require("browserify");
+var buffer = require("gulp-buffer");
 
-gulp.task("default",["html","sass"], function(){
+gulp.task("default",["html","sass", "js"], function(){
     browserSync.init({server: "dist/"});
 
     gulp.watch(["src/scss/*.scss","src/scss/**/*.scss"],["sass"]);    
     gulp.watch(["src/*.html", "src/**/*.html"], ["html"]);
+    gulp.watch(["src/js/*.js", "src/js/**/*.js"], ["js"]);
 })
 
 gulp.task("sass", function(){
@@ -26,3 +30,20 @@ gulp.task("html", function(){
         .pipe(gulp.dest("dist/"))
         .pipe(browserSync.stream());
 })
+
+gulp.task("js", function(){
+    gulp.src("src/js/main.js")
+        .pipe(tap(function(file){ // tap nos permite ejecutar una función por cada fichero seleccionado en gulp.src
+            // reemplazamos el contenido del fichero por lo que nos devuelve browserify pasándole el fichero
+            file.contents = browserify(file.path, {debug: true}) // creamos una instancia de browserify en base al archivo
+                            .transform("babelify", {presets: ["es2015"]}) // traduce nuestro codigo de ES6 -> ES5
+                            .bundle() // compilamos el archivo
+                            .on("error", function(error){ // en caso de error, mostramos una notificación
+                                return notify().write(error);
+                            });
+        }))
+        .pipe(buffer()) // convertimos a buffer para que funcione el siguiente pipe
+       
+        .pipe(gulp.dest("dist/")) // lo guardamos en la carpeta dist
+        .pipe(browserSync.stream()); // recargamos el navegador
+});
