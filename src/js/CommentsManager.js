f640e1e742161eb391d06eef07b1d71e44d8634d
Luis Barriga <barbaclu@gmail.com>
2017-08-23 19:07:49 +0200

    Refactorizamos el tema de la carga de comentarios creando el fichero CommentsManager.js

---
+++ src/js/CommentsManager.js
@@ -0,0 +1,51 @@
+const $ = require("jquery")
+
+export default class CommentsManager{
+
+    constructor(commentService, commentsListUIManager){
+        this.commentService=commentService;
+        this.commentsListUIManager=commentsListUIManager;
+    }
+
+    init(){
+        this.loadComments();
+    }
+    
+    loadComments(){
+        this.commentService.list(comments => {
+            //comprobamos si hay comentarios
+            if (comments.length == 0){
+                this.commentsListUIManager.setEmpty();
+            }else{
+                this.renderComments(comments);
+                this.commentsListUIManager.setIdeal();
+            }
+            
+        },error => {
+            this.commentsListUIManager.setError();
+            console.log("Error al cargar los comentarios", error);
+        });
+    }
+
+    renderComments(comments){
+        let html ="";
+        for (let comment of comments){
+            html += `<article class="comment">
+                            <div class="comment-name">
+                                <span>${comment.nombre}</span>
+                                <span>•</span>
+                                <span>hace 9 años</span>
+                            </div>
+                            <div class="comment-body">
+                                <p> 
+                                    ${comment.comment}
+                                </p>
+                            </div>
+                            <div class="comment-mail">
+                                 <a href="mailto:luis@zerone.es">${comment.mail}</a>
+                            </div>
+                    </article>`;
+        }
+        $(".comments").html(html);
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,10 @@
 window.$ = window.jQuery = require("jquery");
 import CommentsService from "./CommentsService";
 import UiManager from "./UiManager";
+import CommentsManager from "./CommentsManager"
 
 const commentService = new CommentsService("/comentarios/");
 const commentsListUIManager = new UiManager(".comments");
+const commentsManager = new CommentsManager(commentService, commentsListUIManager);
 
-commentService.list(comments => {
-        //comprobamos si hay comentarios
-        if (comments.length == 0){
-            commentsListUIManager.setEmpty();
-        }else{
-            let html ="";
-            for (let comment of comments){
-                html += `<article class="comment">
-                                <div class="comment-name">
-                                    <span>${comment.nombre}</span>
-                                    <span>•</span>
-                                    <span>hace 9 años</span>
-                                </div>
-                                <div class="comment-body">
-                                    <p> 
-                                        ${comment.comment}
-                                    </p>
-                                </div>
-                                <div class="comment-mail">
-                                     <a href="mailto:luis@zerone.es">${comment.mail}</a>
-                                </div>
-                        </article>`;
-            }
-            $(".comments").html(html);
-            commentsListUIManager.setIdeal();
-        }
-        
-    },error => {
-        commentsListUIManager.setError();
-        console.log("Error al cargar los comentarios", error);
-    });
+commentsManager.init();
