655c20129d411400853746c5e0522a6d59162bbf
Luis Barriga <barbaclu@gmail.com>
2017-09-02 20:52:57 +0200

    Preparamos la carga asincrona de los comentarios al hacer  scroll en la página de article.html

---
+++ src/js/article.js
@@ -0,0 +1,47 @@
+import CommentsService from "./CommentsService";
+import UiManager from "./UiManager";
+import CommentsManager from "./CommentsManager";
+import CommentsFormManager from "./CommentsFormManager";
+var isloading=0; 
+var page=0; 
+ 
+const $ = require("jquery")
+$( function() {
+    $( window ).scroll(function() { checkcontent(); });
+ 
+    $( window ).resize(function() { checkcontent(); });
+ 
+    checkcontent();
+    });
+ 
+
+function checkcontent() {
+    if ( isloading==1 ) return;
+ 
+    var el=$(".commentLoader");
+ 
+    if ( $(window).scrollTop()+$(window).height() > el.offset().top )
+        loadcontent( el );
+    }
+ 
+
+function loadcontent( el ) {
+    isloading=1;
+    //Pongo un setTimeout para probar que el funcionamiento es el optimo
+    window.setTimeout(function(){ 
+        const commentService = new CommentsService("/comentarios/");
+        const commentsListUIManager = new UiManager(".comments");
+        
+        const commentsManager = new CommentsManager(commentService, commentsListUIManager, PubSub);
+        commentsManager.init();
+        
+        const commentsFormManager = new CommentsFormManager(".comment-form", commentService, PubSub);
+        commentsFormManager.init();
+
+        document.getElementById('flotante').style.display = '';
+
+    }, 0)
+
+   
+    
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,6 @@
 window.$ = window.jQuery = require("jquery");
-import CommentsService from "./CommentsService";
-import UiManager from "./UiManager";
-import CommentsManager from "./CommentsManager"
-import CommentsFormManager from "./CommentsFormManager"
 import menu from "./menu"
 import articles from "./articles"
+import article from "./article"
 import PubSub from "pubsub-js";
 
-const commentService = new CommentsService("/comentarios/");
-const commentsListUIManager = new UiManager(".comments");
-
-const commentsManager = new CommentsManager(commentService, commentsListUIManager, PubSub);
-commentsManager.init();
-
-const commentsFormManager = new CommentsFormManager(".comment-form", commentService, PubSub);
-commentsFormManager.init();
